1f739a154f397cb764f47c00dfd18ba9d2fe2f53
YashSakhwala <yashsakhwala@gmail.com>
2024-04-19 09:23:12 +0530

    authComplete

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_build_context_synchronously
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -49,10 +49,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
     {"icons": AppImages.privacyPolicy, "name": "Privacy Policy"},
     {"icons": AppImages.terms, "name": "Terms & Condition"},
     {"icons": AppImages.contact, "name": "Contact Us"},
-    {
-      "icons": AppImages.logout,
-      "name": "Logout",
-    },
+    {"icons": AppImages.logout, "name": "Logout"},
   ];
 
   @override
